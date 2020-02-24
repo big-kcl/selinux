ac687fb1b14c331cd03314ae3368b13e871a53ac
Martin Chapman <contact@martinchapman.co.uk>
2020-02-24 03:41:06 +0000

    -- Temporarily host vagrant machine on host's IP address (eventually connect over docker bridge network) -- Fix basic OS permission issue on token file.

--- _vagrant/step3_nr.sh
+++ _vagrant/step3_nr.sh
@@ -5,6 +5,7 @@ sudo make -C /srv/arch-selinux/_vagrant/policies/nr
 sudo semodule -i /srv/arch-selinux/_vagrant/policies/nr/nr.pp
 mkdir /home/vagrant/store
 sudo chcon -t nr_t /home/vagrant/store
-sudo chown -R vagrant:vagrant /home/vagrant/store
 touch /home/vagrant/store/tokens
-sudo echo /srv/arch-selinux/_vagrant/keys/vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/store
+
+sudo cat /srv/arch-selinux/_vagrant/keys/vagrant.pub >> /home/vagrant/.ssh/authorized_keys
